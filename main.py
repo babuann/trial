eab784453975bf0998b114524f4dbff1b406099c
babuann <annmarybabuch@gmail.com>
2024-07-10 17:57:22 +0530

    code file

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ You are a smart assitant so please let me know what is the best way to learn mac
 bedrock=boto3.client(service_name="bedrock-runtime")
 
 payload={
-    
+    "prompt":"[INST]"+prompt+"[INST]",
+    "max_gen_len":512,
+    "temperature":0.3,
+    "top_p":0.9
     
 }
 
 body=json.dumps(payload)
 model_id="meta.llama3-8b-instruct-v1:0"
 
-response=bedrock.invoke_modle(body=body,model_id=model_id,accept="application/json",content_type="application/json")
+response=bedrock.invoke_model(body=body,modelId=model_id,accept="application/json",contentType="application/json")
 response_body=json.loads(response.get("body").read())
 response_text=response_body["generation"]
 print(response_text)
